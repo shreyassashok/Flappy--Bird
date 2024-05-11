bf0bafc80bdf1a19b05a6218aa192bab647d9fac
shreyassashok <79264687+shreyassashok@users.noreply.github.com>
2024-05-11 11:24:08 +0530

    Update bird.js

--- bird.js
+++ bird.js
@@ -1,6 +1,6 @@
 const birdElem = document.querySelector("[data-bird]")
-const BIRD_SPEED = 1.0
-const JUMP_DURATION = 50
+const BIRD_SPEED = 10.0
+const JUMP_DURATION = 100
 let timeSinceLastJump = Number.POSITIVE_INFINITY
 
 export function setupBird() {
